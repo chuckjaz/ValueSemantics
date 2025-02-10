580384fd3977776ffa8b03e206aeb6122b3cc867
Chuck Jazdzewski <chuckj@google.com>
2025-02-10 15:19:32 -0800

    Remove some debugging code

--- src/main.kt
+++ src/main.kt
@@ -1,15 +1,8 @@
 import org.removingalldoubt.values.sharable.sharableSample
 import org.removingalldoubt.values.dataclass.dataClassSample
 import org.removingalldoubt.values.with.withSample
-import kotlin.math.min
 
 fun main() {
-    repeat(100) {
-        val size = it + 1
-        val power = min(32 - (size - 1).countLeadingZeroBits(),  5)
-        println("$size -> $power, effective size ${1 shl power}")
-    }
-
     println("## Data classes ##")
     dataClassSample()
     println()
